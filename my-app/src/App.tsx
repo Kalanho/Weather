2e4d1f24d5abf2597d69cc8075f65f7cb171e4e9
unknown <823meli@gmail.com>
2025-08-18 17:02:21 +0300

    Работа с менеджером состояний

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,27 +5,46 @@ import Location from './Components/Location/Location';
 import Coordinates from './Components/Coordinates/Coordinates';
 ///import Search from './Components/Search/Search';
 import WeatherCard from './Components/WeatherCard/WeatherCard';
-///import Map from './Components/Map/Map';
+//import Map from './Components/Map/Map';
 import General_information from './Components/General_information/General_information';
+import store from './store/store';
+import { observer } from 'mobx-react-lite';
+
+const weatherData = [
+  { day: 1, weatherCondition: "Clear", temperature: 23 },
+  { day: 2, weatherCondition: "Clouds", temperature: 20 },
+  { day: 3, weatherCondition: "Rain", temperature: 22 },
+];
 
 function App() {
 
   return (
     <div className="App">
       <div className='left-container'>
-        <Location city="MINSK" country="BELARUS" />
-        <General_information feel={5} wind={2} humidity={87} weatherCondition="Rain" day={1} temperature={23} />
+        <Location city={store.city} country={store.country} />
+        <General_information feel={store.feel}
+          wind={store.wind}
+          humidity={store.humidity}
+          weatherCondition={store.weatherConditionString}
+          day={store.day}
+          temperature={store.feeltemperature} />
         <div className='days'>
-        <WeatherCard day={1} weatherCondition="Clear"  temperature={23}></WeatherCard>
-        <WeatherCard day={2} weatherCondition="Clouds"  temperature={20}></WeatherCard>
-        <WeatherCard day={1} weatherCondition="Rain"  temperature={22}></WeatherCard>
+          {weatherData.map((weather, index) => (
+            <WeatherCard
+              key={index}
+              day={weather.day}
+              weatherCondition={weather.weatherCondition}
+              temperature={weather.temperature}
+            />
+          ))}
         </div>
       </div>
       <div className='right-container'>
-        <Coordinates latitude={51.5074} longitude={0.1278} ></Coordinates >
+        <Coordinates latitude={store.latitude} longitude={store.longitude} ></Coordinates >
+       
       </div>
     </div>
   );
 }
 
-export default App;
+export default observer(App)
--- my-app/src/Components/Map/Map.tsx
+++ my-app/src/Components/Map/Map.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-
+import './Map.css'
+import 'leaflet/dist/leaflet.css'
 // Определение типа для координат
 const position: [number, number] = [51.505, -0.09];
 
+
 const Map: React.FC = () => {
   return (
-    <MapContainer
-      center={position}
-      zoom={13}
-      scrollWheelZoom={false}
-      style={{ height: '400px', width: '100%' }}
-    >
-      <TileLayer
-        attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div >
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}
+        style={{ height: '400px', width: '100%' }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+
   );
 };
 
